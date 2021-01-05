f46749a40e38b3a9effb57d75889f7f3cac03c14
lornebb <51881266+lornebb@users.noreply.github.com>
2021-01-05 16:46:29 +0100

    added footer, copywrite and personal styling to footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown({ hover: false });
+    $('.dropdown-trigger-main').dropdown();
     $('.parallax').parallax();
     $('input#username, input#password').characterCounter();
+    $("#copyright").text(new Date().getFullYear());
   });
