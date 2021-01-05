5252cddbd49b80d179774635f3968e578d6b3c15
lornebb <51881266+lornebb@users.noreply.github.com>
2021-01-05 14:59:14 +0100

    Added parallex to base page and init'd on scripts.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown({ hover: false });
+    $('.parallax').parallax();
     $('input#username, input#password').characterCounter();
   });
